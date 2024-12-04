a748a130fa0753c39655624e015eb060419dc952
Jianglong Yu <yujianglan98@gmail.com>
2024-12-03 20:57:26 -0600

    channel0-2 images loaded

--- main.js
+++ main.js
@@ -172,7 +172,11 @@ function loadTextures(gl) {
                     image
                 );
 
-                gl.generateMipmap(gl.TEXTURE_2D);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+                // gl.generateMipmap(gl.TEXTURE_2D);
                 resolve();
             };
         });
